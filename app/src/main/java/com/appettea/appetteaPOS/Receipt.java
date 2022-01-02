f02a6752434ddff5ef602204d3e9a7f8c78e4e67
collincebecky <41340260+collincebecky@users.noreply.github.com>
2022-01-01 22:24:08 -0800

    Update Receipt.java

--- app/src/main/java/com/appettea/appetteaPOS/Receipt.java
+++ app/src/main/java/com/appettea/appetteaPOS/Receipt.java
@@ -1,7 +1,19 @@
 package com.appettea.appetteaPOS;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -12,7 +24,8 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Vector;
 
-public class Receipt {
+public class Receipt extends
+    AppCompatDialogFragment{
 
     private HashMap<String,Integer> monitor;
 
@@ -32,6 +45,41 @@ public class Receipt {
         this.monitor = monitor;
         this.storeItemInfo = storeItemInfo;
 
+
+    }
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder= new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_receipt,null);
+
+        builder.setView(view);
+
+
+        TextView textView = view.findViewById(R.id.receipt);
+
+        textView.setText(getReceipt1());
+
+        final AlertDialog dialog = builder.create();
+
+
+        Button print = view.findViewById(R.id.print);
+
+        print.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(context,"No printer available ",Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+
+
+
+        return dialog;
 
     }
     public String getDate(){
@@ -113,26 +161,73 @@ public class Receipt {
         String Header3 ="Thanks for purchasing  from us welcome order "+getTime()+ " item names "+getItemsName() + "total amount  "+ amt;
 
 
-        return Header3;
+        return Header2;
+
+    }
+
+    public String getReceipt1() {
+
+
+        String Header =
+
+                "****** Resturant Management*******\n"
+                        + "Date:     Time: " + getTime() + "\n"
+                        + "         Original Receipt        \n"
+                        + "                                   \n"
+                        + "Name             Qty       Amount\n"
+
+                        + "--------------------------------------------------------\n"
+                        + getItemsName() + " \n"
+                        + "-------------------------------------------------------\n"
+                        + "Total Amount:      Ksh. " + amt + " \n"
+                        + "-------------------------------------------------------\n"
+                        + "For Further Detail                \n"
+                        + "Please Call/Whatsapp 0796770664\n"
+                        + "-------------------------------------------------------\n"
+                        + "       Software Developed by :     \n "
+                        + "             Appettea            \n"
+                        + "**************************************\n"
+                        + "            Thank You             \n"
+                        + "-------------------------------------------------------\n";
+
+
+
+
+
+
+        return  Header;
+
 
     }
 
     private String getItemsName(){
 
+
+
         String items = "";
 
         int i = 0;
-        /*
-        for (String n:monitor.keySet()
+
+
+
+        for (String id:monitor.keySet()
              ) {
 
-            int amount = monitor.get(n);
 
-            Vector<String> b = storeItemInfo.get(n);
 
-            int t_amount = (int)Integer.parseInt(b.get(5))*amount;
+            int val = monitor.get(id);
 
-            String h = b.get(1)+"      "+amount+"      "+t_amount;
+            String price = (String) storeItemInfo.get(id).elementAt(7);
+
+            String name = (String) storeItemInfo.get(id).elementAt(1);
+
+
+
+            int t_amount= Integer.parseInt(price)*val;
+
+           // int t_amount =Integer.parseInt((String)b.get(7))*amount;
+
+            String h = name+"      "+val+" x "+price+"      "+t_amount;
 
             amt+=t_amount;
 
@@ -146,7 +241,7 @@ public class Receipt {
 
         }
 
-         */
+
 
         return items;
 
